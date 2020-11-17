94cfcb3fdfb5af1cc5e6d20ffaa66e01346568e6
LusineKaryan <72447443+LusineKaryan@users.noreply.github.com>
2020-11-17 23:48:34 +0100

    17nov

---
+++ exo4/js/main.js
@@ -0,0 +1,10 @@
+
+let el = document.getElementById('myText');
+function character(){
+if (character == character.toUpperCase()) {
+ alert ('upper case true');
+}
+if (character == character.toLowerCase()){
+ alert ('lower case true');
+}
+}
